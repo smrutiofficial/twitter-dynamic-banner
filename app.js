f1820ede3289e04a40b915c890dbc665a121098c
smrutiofficial <smrutiprakshrout3@gmail.com>
2022-03-06 22:39:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,179 @@
+const axios = require('axios');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const sharp = require('sharp');
+const client = require("twitter-api-client");
+const express = require("express");
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+//****************KEYS******************* //
+
+const twitterClient = new client.TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY,
+    apiSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+// *************************************** //
+
+async function saveImage(follower) {
+    let fileName = `${follower.screen_name}.png`;
+    await processImage(follower.profile_image_url_https, fileName, true, { width: 60, height: 60 });
+}
+async function saveImageAndData(followers) {
+    // console.log(followers);
+    try {
+        let index = 0;
+        const image_data = [];
+        for (let follower of followers.users) {
+            let fileName = `${follower.screen_name}.png`;
+            await saveImage(follower)
+            .then(() => {
+                if (fs.existsSync(fileName)) {
+                    const follower_avatar = {
+                        input: fileName,
+                        top: 282,
+                        left: parseInt(`${280 + 70 * index}`),
+                    };
+                    image_data.push(follower_avatar);
+                    index++;
+                }
+            });
+            if (image_data.length == 3) {
+                break;
+            }
+        }
+        console.log(image_data);
+        return image_data;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function processImage(url, image_path, isUserImage, resizeData) {
+    try {
+        await axios({
+            url,
+            responseType: "arraybuffer",
+        }).then((response) =>
+            new Promise((resolve) => {
+                if (isUserImage == true) {
+                    const rounded_corners = new Buffer.from(
+                        '<svg><rect x="0" y="0" width="50" height="50" rx="50" ry="50"/></svg>');
+                    resolve(
+                        sharp(response.data)
+                            .resize(resizeData.width, resizeData.height)
+                            .composite([
+                                {
+                                    input: rounded_corners,
+                                    blend: "dest-in",
+                                },
+                            ])
+                            .png()
+                            .toFile(image_path)
+                    );
+                } else {
+                    resolve(
+                        sharp(response.data)
+                            .resize(resizeData.width, resizeData.height)
+                            .png()
+                            .toFile(image_path)
+                    );
+                }
+            })
+        )
+            .catch((err) => {
+                console.log("error in getting profile url");
+            });
+    }
+    catch (err) {
+        console.log(image_path + " " + err);
+    }
+}
+
+async function drawImage(image_data) {
+    try {
+        let twitterFile = "background.png";
+
+
+        // Logic for changing theme based on time
+
+        const hour = new Date().getHours();
+        const theme = ["morning.png", "afternoon.png", "evening.png", "night.png"];
+        twitterFile = theme[0];
+        console.log(hour);
+        if (hour < 12 && hour > 6) twitterFile = theme[0];
+        else if (hour < 18 && hour >= 12) twitterFile = theme[1];
+        else if (hour < 22 && hour >= 18) twitterFile = theme[2];
+        else if (hour <= 24 || hour <= 6) twitterFile = theme[3];
+
+        new Promise((resolve) => {
+            resolve(sharp("banner/" + twitterFile)
+                .composite(image_data)
+                .toFile("twitterBanner.png")
+            );
+        })
+    } catch (error) {
+        console.log("Catch" + error);
+    }
+}
+
+async function uploadBanner() {
+    try {
+        const base64Banner = fs.readFileSync("twitterBanner.png", {
+            encoding: "base64",
+        });
+        await twitterClient.accountsAndUsers.accountUpdateProfileBanner({ banner: base64Banner });
+    }
+
+    catch (err) {
+        console.log("error in uploading banner " + err);
+    }
+}
+
+async function deleteFiles(files) {
+    try {
+        files.forEach((file) => {
+            if (file.input.includes(".png")) {
+                fs.unlinkSync(file.input);
+            }
+        });
+    } catch (err) {
+        console.error("Not able to delete" + err);
+    }
+}
+
+
+async function getFollowers() {
+    const params = {
+        screen_name: 'SmrutiPrakashR3',
+        count: 3
+    }
+    await twitterClient.accountsAndUsers.followersList(params)
+    .then((follower) => {
+        saveImageAndData(follower)
+        .then((image_data) => {
+                drawImage(image_data).then(() => {
+                    console.log(image_data);
+                    uploadBanner()
+                    .then(() => {
+                        deleteFiles(image_data);
+                    });
+                });
+            });
+        })
+        .catch(() => {
+            console.log("error in save image");
+        });
+    };
+getFollowers();
+app.get('/', (req, res) => {
+    res.send("banner");
+});
+
+app.listen(port, () => {
+    console.log(`server listening at ${port}`);
+});
